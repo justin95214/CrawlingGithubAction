05def50ebf5188ff44b19e3ef42d7bc9ee5dd85d
Duhyeuk Chang <54429808+justin95214@users.noreply.github.com>
2024-04-26 00:29:24 +0900

    Update string_resource.py

--- string_resource.py
+++ string_resource.py
@@ -3,4 +3,4 @@ id_edit = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/div[2]/di
 ps_edit = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/div[2]/div/div[2]/input'
 login_button = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/div[2]/button'
 work_activity= '/html/body/div[2]/div[2]/div/div/div/ul/li[3]/a/span/strong
-test = '/html/body/div[2]/div[1]/div/div[3]/div/ul[1]/li[1]'
+work_daily = '/html/body/div[2]/div[2]/div/div/div/ul/li[3]/ul/li[3]/a'
