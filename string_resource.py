c5a2bd15ea26b0afd63150ea8aba4ee127c4714d
Duhyeuk Chang <54429808+justin95214@users.noreply.github.com>
2024-04-26 00:31:02 +0900

    Update string_resource.py

--- string_resource.py
+++ string_resource.py
@@ -2,5 +2,5 @@ nongshim_group_list = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[
 id_edit = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/div[2]/div/div[1]/input'
 ps_edit = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/div[2]/div/div[2]/input'
 login_button = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/div[2]/button'
-work_activity= '/html/body/div[2]/div[2]/div/div/div/ul/li[3]/a/span/strong
+work_activity= '/html/body/div[2]/div[2]/div/div/div/ul/li[3]/a/span/strong'
 work_daily = '/html/body/div[2]/div[2]/div/div/div/ul/li[3]/ul/li[3]/a'
