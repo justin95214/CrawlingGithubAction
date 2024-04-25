e006bad5f47b0eb3a380af2d95920995cd8cf8e7
Duhyeuk Chang <54429808+justin95214@users.noreply.github.com>
2024-04-25 23:22:36 +0900

    Create string_source.py

---
+++ string_source.py
@@ -0,0 +1,6 @@
+nongshim_group_list = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/select'
+id_edit = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/div[2]/div/div[1]/input'
+ps_edit = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/div[2]/div/div[2]/input'
+login_button = '/html/body/form[1]/div[3]/div/div[2]/div[2]/fieldset/div[1]/div[2]/button'
+work_activity= '/html/body/div[2]/div[2]/div/div/div/ul/li[3]/'
+test = '/html/body/div[2]/div[1]/div/div[3]/div/ul[1]/li[1]'
