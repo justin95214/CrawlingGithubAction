b03731fb0b3389011104b849a24e3f88a2b29d50
Duhyeuk Chang <54429808+justin95214@users.noreply.github.com>
2024-04-26 08:43:42 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -60,6 +60,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 # '업무활동' 메뉴 클릭
 # 요소가 나타날 때까지 대기
