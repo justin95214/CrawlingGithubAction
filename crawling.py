ccdff095f326f57de698891e5005dcd76c8fc439
Duhyeuk Chang <54429808+justin95214@users.noreply.github.com>
2024-04-27 10:46:17 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -37,14 +37,14 @@ company_select = Select(driver.find_element(By.XPATH, string_resource.nongshim_g
 company_select.select_by_visible_text('농심엔지니어링')
 
 
-
+print('login in window')
 # 아이디와 비밀번호 입력
 username = driver.find_element(By.XPATH,  string_resource.id_edit)
 password = driver.find_element(By.XPATH, string_resource.ps_edit)
 
-print('login in')
 username.send_keys("2022037")
 password.send_keys("j1995214!")
+
 
 # 로그인 버튼 클릭
 login_button = driver.find_element(By.XPATH, string_resource.login_button)
@@ -76,11 +76,28 @@ try:
     #https://www.nsgportal.net/ekp/workDiary/workDiary.do?cmd=eng_wd_write
     driver.get("http://www.nsgportal.net/ekp/workDiary/workDiary.do?cmd=eng_wd_write")
 
-    # 로그인 버튼 클릭
+    # 업무 일지 화면 확인
     work_title = driver.find_element(By.XPATH, '/html/body/div/div[1]/h3').text
     print(work_title)
 
+    #프로젝트 제목 입력
+    project_title = driver.find_element(By.XPATH, '/html/body/div/div[3]/form/table[1]/tbody/tr[1]/td[1]/input')
+    project_title.send_keys("VIS 검사프로그램 개발")
+
+    # 검색 버튼 클릭
+    search_button = driver.find_element(By.XPATH, '/html/body/div/div[3]/form/table[1]/tbody/tr[3]/td[1]/a[1]')
+    search_button.click()
     
+    #프로젝트 코드 검색 창
+    # 새로 열린 탭으로 이동
+    driver.switch_to.window(driver.window_handles[2])
+    driver.get('https://www.nsgportal.net/ekp/workDiary/workDiary.do?cmd=eng_wd_pj_pop')
+
+    # 프로젝트 검색 창 이동 확인
+    project_window = driver.find_element(By.XPATH, '/html/body/form/div/div[1]/h3').text
+    print(project_window)
+
+
 except TimeoutException:
     print("요청한 요소를 찾는 데 시간이 너무 오래 걸립니다.")
 except NoSuchElementException:
